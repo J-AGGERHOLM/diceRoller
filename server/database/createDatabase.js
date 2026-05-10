cef57ecb2a66fdaacbd3136391a1f9044386ea94
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-05-10 12:26:36 +0200

    Added more test characters

--- server/database/createDatabase.js
+++ server/database/createDatabase.js
@@ -5,6 +5,7 @@ import { hashPassword } from "../util/bcryptCompare.js";
 const deleteMode = process.argv.includes("--delete");
 
 if (deleteMode) {
+  await db.execute(`DROP TABLE IF EXISTS characters;`);
   await db.execute(`DROP TABLE IF EXISTS users;`);
 }
 
@@ -25,6 +26,25 @@ await db.execute(`
     );
 `);
 
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS characters (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    name VARCHAR(100),
+    level INT NOT NULL DEFAULT 1,
+    race VARCHAR(50),
+    class_name VARCHAR(50),
+    str_score INT,
+    dex_score INT,
+    con_score INT,
+    int_score INT,
+    wis_score INT,
+    cha_score INT,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    )
+`);
+
 //DML
 //seeding
 if (deleteMode) {
@@ -40,4 +60,16 @@ if (deleteMode) {
     .execute(`INSERT INTO users (username, password, role) VALUES (?, ?, ?)`,
     ['user01', hashedPassword, 'User']
     );
+
+  await db.execute(
+  `INSERT INTO characters (user_id, name, level, race, class_name, str_score, dex_score, con_score, int_score, wis_score, cha_score)
+   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+  [1, 'Origrin Doomhammer', 1, 'Human', 'Fighter', 15, 12, 13, 8, 14, 11]
+  );
+
+  await db.execute(
+  `INSERT INTO characters (user_id, name, level, race, class_name, str_score, dex_score, con_score, int_score, wis_score, cha_score)
+   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+  [1, 'Gorrum Ashveil', 5, 'Dwarf', 'Cleric', 14, 8, 16, 10, 17, 9]
+  );
 }
