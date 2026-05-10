b59720c6aaddd548be9022a44fdb5396d3302f0c
J-AGGERHOLM <jajo0004@stud.kea.dk>
2026-05-10 14:25:06 +0200

    create character endpoint

--- server/routers/charactersRouter.js
+++ server/routers/charactersRouter.js
@@ -3,6 +3,7 @@ import db from "../database/connection.js"
 
 const router = Router();
 
+// ================== character endpoints ================== //
 //all characters for a user
 router.get("/characters/:userid", async (req, res) => {
 
@@ -16,6 +17,7 @@ router.get("/characters/:userid", async (req, res) => {
 
 });
 
+//get specific character
 router.get("/characters/:userid/:characterid", async (req, res) => {
 
     const { userid, characterid } = req.params;
@@ -28,7 +30,29 @@ router.get("/characters/:userid/:characterid", async (req, res) => {
     res.send(response);
 });
 
+// create new character
+router.post("/characters", async (req, res) => {
+    const {
+        user_id, name, level, race, class_name, 
+        str_score, dex_score, con_score, int_score, 
+        wis_score, cha_score 
+    } = req.body;
 
+    await db.execute(`INSERT INTO characters (
+        user_id, name, level, race, class_name)
+        VALUES (?, ?, ?, ?, ?)`,
+    [user_id, name, level, race, class_name]);
+
+    res.send({ data: `${name} added to the database`})
+
+});
+
+//update character
+/*
+router.put("/characters/:userid/:characterid"), async (req, res) => {
+
+}
+*/
 
 
 
